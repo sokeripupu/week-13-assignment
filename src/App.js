57e24db6c989c2c2cdb1e94f2d47c12ed79856f1
Your Name <you@example.com>
2023-01-27 22:49:38 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/login/Login";
 import Navbar from "./components/navigation/Navbar";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState("login");
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  };
 
   return (
     <div className="App">
